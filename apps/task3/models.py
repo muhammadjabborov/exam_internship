6037d1f73831484697414b6a7c991348d7c2e836
muhammadjabborov <zabboorovmuhammad291@gmail.com>
2023-04-11 18:15:09 +0500

    fix : migrations

--- apps/task3/models.py
+++ apps/task3/models.py
@@ -25,7 +25,7 @@ class Announcement(BaseModel):
     image = ImageField(upload_to='%Y/%m/%d')
     title = CharField(max_length=255)
     description = TextField()
-    price = DecimalField(decimal_places=2, max_digits=6)
+    price = DecimalField(decimal_places=2, max_digits=10)
 
     def __str__(self):
         return self.title
